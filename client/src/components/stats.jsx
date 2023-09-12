0afa5b8736b681f34b66d1711220ca90b0bf0e98
Isai Quiroz <isaiquiroz@Isais-MacBook-Pro-2.local>
2023-09-12 10:16:29 -0700

    resolve

---
+++ client/src/components/stats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const StatsGraph = ({  }) => {
+  return (
+    <div>
+      <h2>Stats Graph</h2>
+      <Chart data={typeDefs} /> {$fastMove}
+    </div>
+  );
+};
+
+export default StatsGraph;
--- client/src/pages/addPokemon.jsx
+++ client/src/pages/addPokemon.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_MATCHUPS } from "../utils/queries";
-import { NormalLoginForm } from "../components/form";
 
 import React from "react";
 import { Button, Layout, Menu, Typography } from "antd";
@@ -9,7 +8,6 @@ const { Header, Content } = Layout;
 const Add = () => {
 	return (
 		<Content className="content">
-			<NormalLoginForm />
 			<div className="box">
 				<h2>Add Pokemon</h2>
 				<p>Pokemon search</p>
